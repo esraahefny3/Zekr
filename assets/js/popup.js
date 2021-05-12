70268d68e395f2d3ae530a545a0aac7742185c89
heba-elsafty <heba.m.elsafty@gmail.com>
2021-05-12 05:29:19 +0200

    enhance popup design

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,30 +2,30 @@ var bgpage = chrome.extension.getBackgroundPage();
 var click_status = false;
 
 
-$(function($) {
+$(function ($) {
   waitResponse();
-  $('#settings-btn').click(function() {
+  $('#settings-btn').click(function () {
     chrome.runtime.sendMessage({
       openSettings: true
     }, null);
   });
-  $('#user-profile-btn').click(function() {
+  $('#user-profile-btn').click(function () {
     open_new_tab("user-profile.html");
   });
-  $('#dashboard-btn').click(function() {
+  $('#dashboard-btn').click(function () {
     open_new_tab("dashboard.html");
   });
   run_popup();
 });
 
 function waitResponse() {
-  $('#dynamicDiv').hide();
+  // $('#dynamicDiv').hide();
   $('.user-tags').hide();
 
 }
 
 function showResponse() {
-  $('#dynamicDiv').show();
+  // $('#dynamicDiv').show();
   $('.user-tags').show();
 }
 
@@ -37,22 +37,33 @@ function run_popup() {
 
   chrome.runtime.sendMessage({
     getAnotherRandomZekrOverlay: true
-  }, function(response) {
+  }, function (response) {
 
     // Script injected, we can proceed
     if (response && response.done) {
       console.log(response);
       let zekr = document.getElementById("zekr");
       let fadl = document.getElementById("fadl");
-      var higriDate = document.getElementById("higri-date");
+
       // var higriDate = document.getElementById("higri_lang");
       var popup_content = document.getElementById("popup_content");
       var sheet = document.createElement('style');
       document.body.appendChild(sheet);
 
-      zekr.innerHTML = `<i class="fas fa-quote-right fa-xs"></i> ${response.zekrData.zekr} <i class="fas fa-quote-left fa-xs"></i> `;
+      zekr.innerHTML = `<i class="fas fa-quote-right fa-xs"></i>
+                        ${response.zekrData.zekr}
+                        <i class="fas fa-quote-left fa-xs"></i> `;
+
+      if (response.higriDate === "") {
+        return null;
+      } else {
+        const higriDate = document.getElementById("higri-date");
+        higriDate.innerHTML = response.higriDate;
+      }
+
       fadl.innerHTML = response.zekrData.fadl;
-      higriDate.innerHTML = response.higriDate;
+
+
 
       // response_date.innerHTML = response.date;
       showResponse();
